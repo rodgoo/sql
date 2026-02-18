7fec24e55e05c84399faf8102d5fb4cac92bfe3c
Rodrigo Carvalho <rodrigocarvalho052006@gmail.com>
2026-02-18 01:20:38 -0300

    Primeiro projeto de tabelas em SQL

---
+++ sql/sql_aula_01.sql
@@ -0,0 +1,11 @@
+use cadastro_pessoas;
+CREATE table cadastro_pessoas (
+nome varchar(30),
+idade tinyint(3),
+sexo char(1),
+peso float,
+altura float,
+nacionalidade varchar(20)
+);
+
+describe cadastro_pessoas
